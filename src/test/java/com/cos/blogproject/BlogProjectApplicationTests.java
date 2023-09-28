3550e5271a5369667d8837c4d34007e00cbb1bbb
saki8661 <saki8661@naver.com>
2023-09-28 15:35:54 +0900

    Spring, MySQL연동

---
+++ src/test/java/com/cos/blogproject/BlogProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.blogproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
